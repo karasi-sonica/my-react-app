e8412808c825f4f278f47fffa0dac0d5db568c59
karasi-sonica <karasisonica@gmail.com>
2025-02-21 09:39:48 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Sonica</h1>
+      <p>Email: sonica@gmail.com</p>
+      <p>Phone: +1234567890</p>
+    </div>
+  );
+}
+
+export default App;
